6dedb52dee59cca80c79883576c478eb760121ee
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-10-20 10:13:45 +0900

    feature-controller-actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
